edee1c6a6a8097d5b16f2efb716df2276480c64b
Dubenis L <43077233+dubis78@users.noreply.github.com>
2020-11-22 17:22:21 -0500

    mock changes

--- src/utils/mocks/Data.js
+++ src/utils/mocks/Data.js
@@ -1,8 +1,3 @@
-import img2 from '../../img/2.png';
-import img3 from '../../img/3.png';
-import img4 from '../../img/4.png';
-import img5 from '../../img/5.png';
-
 export const Data={
     navbarData:[
         {
